9599ff8246ee85cdd5ba1c1dc0ad6d07220778b0
Sani Mustapha <mustapha3341@gmail.com>
2020-12-13 14:46:38 +0000

    added .env and firebase

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA750qcmFW1_OCq1bgLPRQ546m1S2wT7BE",
+    authDomain: "awesome-note-app-22f7f.firebaseapp.com",
+    projectId: "awesome-note-app-22f7f",
+    storageBucket: "awesome-note-app-22f7f.appspot.com",
+    messagingSenderId: "947207950202",
+    appId: "1:947207950202:web:916d663f7258f1e2a650c3",
+};
